81431d961a1e9d10accf75bf79364d3f9a2c4b49
Roberto-Aguirre <ivanaguirre02@hotmail.com>
2024-02-11 16:14:26 -0600

    Inicio de clase 5 minuto 25

---
+++ Clase 5/class5.py
@@ -0,0 +1,21 @@
+#Funciones
+
+# def nombre_funcion():
+    #realizar la funcion
+
+# def print_number():
+#     for number in range(1,11):
+#         print(number)
+
+# print_number()S
+
+## Si la funcion realiza una o más tareas debemos extraerlas y realizar otras funciones
+##Los datos de entrada son argumentos = Argumentos -> Parametros
+
+
+
+def suma(num1,num2):
+    result = num1+num2
+    print(result)
+
+suma(20,20)
